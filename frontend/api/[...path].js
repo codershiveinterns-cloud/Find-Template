ffe958c44983bb81b868d3637d5791f8be1e4246
Rahulsharmai4 <rahulsharma@icore.sg>
2026-06-27 17:23:26 +0530

    one folder

--- frontend/api/[...path].js
+++ frontend/api/[...path].js
@@ -1,5 +1,5 @@
-import { app } from '../src/app.js';
-import { connectDB } from '../src/config/db.js';
+import { app } from '../backend/src/app.js';
+import { connectDB } from '../backend/src/config/db.js';
 
 let connectionPromise;
 
